f5c4d93734b9ea6c686ec4b557054dda4d6ee8ea
DhruvPaghdal <dhruvpaghdal2@gmail.com>
2025-03-21 13:49:10 -0400

    Added Base Module For Backend

---
+++ backend/src/main/java/com/example/mobilePlanAPI/MobilePlanApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.mobilePlanAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilePlanApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobilePlanApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/mobilePlanAPI/MobilePlanApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mobilePlanAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobilePlanApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
